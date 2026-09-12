a7d5b8a6e3dd3a23e2d97973f53282f958763fbc
Robin Mordasiewicz <robin@mordasiewicz.com>
2026-09-12 18:39:54 -0400

    style(remote-control): format moved memory lookup

--- packages/coding-agent/src/remote-control/session.ts
+++ packages/coding-agent/src/remote-control/session.ts
@@ -830,11 +830,8 @@ export class RemoteSession {
 					};
 					const settings = this.target.settings;
 					const userKnowledge = settings
-						? ((await readMemorySummary(
-								settings.getAgentDir(),
-								settings,
-								this.target.sessionManager.getCwd(),
-							)) ?? "")
+						? ((await readMemorySummary(settings.getAgentDir(), settings, this.target.sessionManager.getCwd())) ??
+							"")
 						: "";
 					this.#assertCurrent(epoch);
 					return { ...snapshot, userKnowledge };
